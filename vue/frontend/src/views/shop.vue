65798e9694460573dfdf008289547de229f564c6
Jake Anderson <Maceto@dyn-169-226-230-186.wireless.albany.edu>
2019-12-06 03:22:28 -0500

    added a bunch of shop im ready to be done

--- vue/frontend/src/views/shop.vue
+++ vue/frontend/src/views/shop.vue
@@ -2,12 +2,63 @@
   <div class="shop">
     <navbar :middleText='this.mid' />
       <h1 class='cardS'> Shop Balance: {{ shopScore }}</h1>
+      <div class='cardD' v-if='this.showBuy'>{{ shopResp }} You bought something</div>
       <div class="shopGrid">
-        <div class='card'>
+        <div class='card si1'>
           <h1>Item titme</h1>
           <button @click='buy(item1, 1)'>Buy Item</button>
         </div>
-    </div>
+        <div class='card si2'>
+          <h1>Item titme</h1>
+          <button @click='buy(item1, 1)'>Buy Item</button>
+        </div>
+        <div class='card si3'>
+          <h1>Item titme</h1>
+          <button @click='buy(item1, 1)'>Buy Item</button>
+        </div>
+        <div class='card si4'>
+          <h1>Item titme</h1>
+          <button @click='buy(item1, 1)'>Buy Item</button>
+        </div>
+        <div class='card si5'>
+          <h1>Item titme</h1>
+          <button @click='buy(item1, 1)'>Buy Item</button>
+        </div>
+        <div class='card si6'>
+          <h1>Item titme</h1>
+          <button @click='buy(item1, 1)'>Buy Item</button>
+        </div>
+        <div class='card si7'>
+          <h1>Item titme</h1>
+          <button @click='buy(item1, 1)'>Buy Item</button>
+        </div>
+        <div class='card si8'>
+          <h1>Item titme</h1>
+          <button @click='buy(item1, 1)'>Buy Item</button>
+        </div>
+        <div class='card si9'>
+          <h1>Item titme</h1>
+          <button @click='buy(item1, 1)'>Buy Item</button>
+        </div>
+        <div class='card si10'>
+          <h1>Item titme</h1>
+          <button @click='buy(item1, 1)'>Buy Item</button>
+        </div>
+        <div class='card si11'>
+          <h1>Item titme</h1>
+          <button @click='buy(item1, 1)'>Buy Item</button>
+        </div>
+        <div class='card si12'>
+          <h1>Item titme</h1>
+          <button @click='buy(item1, 1)'>Buy Item</button>
+        </div>
+        <div class='divider'></div>
+        <div class="cardS ep">
+          <h1>Ways to Earn Money:</h1>
+          <p>Give Red Team a Compliment - 100 Points</p>
+          <p>Lockpicking Challenges - 200 Points</p>
+          </div>
+      </div>
   </div>
 </template>
 
@@ -24,6 +75,8 @@ export default {
     return {
       mid: 'Shop',
       teamName: '',
+      shopResp: '',
+      showBuy: false,
       jwt: '',
       shopScore: '',
       item1: 'LIAM BOIIIIII'
@@ -38,7 +91,10 @@ export default {
   methods: {
     async buy (item, price) {
       const response = await auth.transaction(this.teamName, this.jwt, item, price)
-      console.log(response)
+      this.shopResp = response.data.message
+      this.showBuy = true
+      const responseS = await ti.getScores(this.teamName)
+      this.shopScore = responseS.data.out.shopScore
     }
   }
 }
@@ -48,5 +104,64 @@ export default {
 @import '/../style/var.scss';
 .shop{
   margin-top: 40px;
+}
+.divider{
+  grid-area: d;
+  height: 3px;
+  background-color: $oneColor;
+  width: 100%;
+}
+.shopGrid{
+  padding: 1em;
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr;
+  grid-gap: 2em;
+  grid-template-rows: auto;
+  grid-template-areas:
+    "si1 si2 si3 si4"
+    "si5 si6 si7 si8"
+    "si9 si10 si11 si12"
+    "d d d d"
+    "ep ep ep ep";
+}
+
+.ep{
+  grid-area: ep
+}
+.si1{
+  grid-area: si1;
+}
+.si2{
+  grid-area: si2;
+}
+.si3{
+  grid-area: si3;
+}
+.si4{
+  grid-area: si4;
+}
+.si5{
+  grid-area: si5;
+}
+.si6{
+  grid-area: si6;
+}
+.si7{
+  grid-area: si7;
+}
+.si8{
+  grid-area: si8;
+}
+.si9{
+  grid-area: si9;
+}
+.si10{
+  grid-area: si10;
+}
+.si11{
+  grid-area: si11;
+}
+.si12{
+  grid-area: si12;
 }
 </style>
